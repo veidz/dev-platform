66c1a2bf5249c680738197498939a9b3e2eb4522
veidz <joaovictorveidz@gmail.com>
2025-11-09 01:53:52 -0300

    feat(ui): add button component

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,2 +1,13 @@
-// Placeholder UI entrypoint
-export const noop = (): undefined => undefined
+export type { ButtonProps } from './components/ui/button'
+export { Button, buttonVariants } from './components/ui/button'
+export {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from './components/ui/card'
+export { Input } from './components/ui/input'
+export { Label } from './components/ui/label'
+export { cn } from './lib/utils'
