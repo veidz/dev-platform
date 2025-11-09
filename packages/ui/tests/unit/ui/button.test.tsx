e0291ab35e49c80ef3d3f39b5a49035965d84fc1
veidz <joaovictorveidz@gmail.com>
2025-11-09 19:14:28 -0300

    test(ui): ensure Button renders children correctly

--- packages/ui/tests/unit/ui/button.test.tsx
+++ packages/ui/tests/unit/ui/button.test.tsx
@@ -3,10 +3,12 @@ import { describe, expect, it } from '@jest/globals'
 import { render, screen } from '@testing-library/react'
 
 describe('Button', () => {
-  it('renders correctly', () => {
-    render(<Button>Click me</Button>)
-    const element = screen.getByText('Click me')
-    expect(element).toBeDefined()
-    expect(element.textContent).toBe('Click me')
+  describe('rendering', () => {
+    it('renders children correctly', () => {
+      render(<Button>Click me</Button>)
+      const element = screen.getByText('Click me')
+      expect(element).toBeDefined()
+      expect(element.textContent).toBe('Click me')
+    })
   })
 })
