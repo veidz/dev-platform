03520c5c6caa3b7e813f15af0715e39f7bf95348
veidz <joaovictorveidz@gmail.com>
2025-11-11 17:16:09 -0300

    test(ui): add jest axe to setup

--- packages/ui/tests/setup.ts
+++ packages/ui/tests/setup.ts
@@ -1,4 +1,7 @@
 import '@testing-library/jest-dom'
+import { toHaveNoViolations } from 'jest-axe'
+
+expect.extend(toHaveNoViolations)
 
 global.ResizeObserver = class ResizeObserver {
   observe(): void {}
