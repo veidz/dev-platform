0a39035e005a8fba2a33e879b34eea71ce325d4a
veidz <joaovictorveidz@gmail.com>
2025-11-11 21:35:45 -0300

    test(ui): ensure RadioGroup renders multiple radio items

--- packages/ui/tests/unit/ui/radio-group/radio-group.test.tsx
+++ packages/ui/tests/unit/ui/radio-group/radio-group.test.tsx
@@ -12,5 +12,17 @@ describe('RadioGroup', () => {
       const radioGroup = screen.getByRole('radiogroup')
       expect(radioGroup).toBeInTheDocument()
     })
+
+    it('should render multiple radio items', () => {
+      render(
+        <RadioGroup>
+          <RadioGroupItem value="option-1" id="opt-1" />
+          <RadioGroupItem value="option-2" id="opt-2" />
+          <RadioGroupItem value="option-3" id="opt-3" />
+        </RadioGroup>,
+      )
+      const radios = screen.getAllByRole('radio')
+      expect(radios).toHaveLength(3)
+    })
   })
 })
