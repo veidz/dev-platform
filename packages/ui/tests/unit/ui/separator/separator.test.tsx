c71adf302f070637da88b8ee55c6e791e53bd240
veidz <joaovictorveidz@gmail.com>
2025-11-14 02:14:57 +1300

    test(ui): ensure Separator renders separator element

---
+++ packages/ui/tests/unit/ui/separator/separator.test.tsx
@@ -0,0 +1,13 @@
+import { Separator } from '@/components/ui/separator'
+import { render } from '@testing-library/react'
+
+describe('Separator', () => {
+  describe('Rendering', () => {
+    it('should render separator element', () => {
+      const { container } = render(<Separator />)
+
+      const separator = container.querySelector('[data-orientation]')
+      expect(separator).toBeInTheDocument()
+    })
+  })
+})
