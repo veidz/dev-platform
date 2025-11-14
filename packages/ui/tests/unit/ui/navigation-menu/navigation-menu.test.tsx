6bd20c603e46c030194c2e70eefb31ab45e2594b
veidz <joaovictorveidz@gmail.com>
2025-11-14 11:44:03 -0300

    test(ui): ensure NavigationMenu renders navigation menu

---
+++ packages/ui/tests/unit/ui/navigation-menu/navigation-menu.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react'
+
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from '@/components/ui/navigation-menu/navigation-menu'
+
+describe('NavigationMenu', () => {
+  describe('Rendering', () => {
+    it('should render navigation menu', () => {
+      render(
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuLink href="/home">Home</NavigationMenuLink>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>,
+      )
+
+      expect(screen.getByText('Home')).toBeInTheDocument()
+    })
+  })
+})
