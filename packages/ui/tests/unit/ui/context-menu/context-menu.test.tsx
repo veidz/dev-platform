3c1ecf0d54182c7375e8bdce1b17b7413d2572bd
veidz <joaovictorveidz@gmail.com>
2025-11-20 18:11:26 -0300

    test(ui): ensure ContextMenu renders trigger element

---
+++ packages/ui/tests/unit/ui/context-menu/context-menu.test.tsx
@@ -0,0 +1,24 @@
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuTrigger,
+} from '@/components/ui/context-menu/context-menu'
+import { render, screen } from '@testing-library/react'
+
+describe('ContextMenu', () => {
+  describe('Rendering', () => {
+    it('should render trigger element', () => {
+      render(
+        <ContextMenu>
+          <ContextMenuTrigger>Right click me</ContextMenuTrigger>
+          <ContextMenuContent>
+            <ContextMenuItem>Item 1</ContextMenuItem>
+          </ContextMenuContent>
+        </ContextMenu>,
+      )
+
+      expect(screen.getByText('Right click me')).toBeInTheDocument()
+    })
+  })
+})
