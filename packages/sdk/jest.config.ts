d8c60d96885aa6508c278db483df1c3226f5c049
veidz <joaovictorveidz@gmail.com>
2025-11-24 12:33:56 -0300

    chore(sdk): add jest setup

---
+++ packages/sdk/jest.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/*.test.ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.types.ts',
+    '!src/**/index.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^ky$': '<rootDir>/tests/__mocks__/ky.ts',
+  },
+}
+
+export default config
