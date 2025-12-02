f5e192c77676bfb3211020f824c5cf56eb4d6c52
veidz <joaovictorveidz@gmail.com>
2025-12-02 14:59:41 -0300

    feat(database): add base Abstract repository

---
+++ packages/database/src/repositories/base/index.ts
@@ -0,0 +1,8 @@
+export { AbstractRepository } from './base.repository'
+export type {
+  BaseRepository,
+  FindManyOptions,
+  PaginatedResult,
+  PaginationOptions,
+} from './base.repository.types'
+export { DEFAULT_LIMIT, DEFAULT_PAGE, MAX_LIMIT } from './base.repository.types'
