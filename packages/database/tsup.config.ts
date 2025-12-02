c7e8e116951df6cf0f301669a0eb7433313e879b
veidz <joaovictorveidz@gmail.com>
2025-12-02 16:03:09 -0300

    chore(database): add path aliases and export paths

--- packages/database/tsup.config.ts
+++ packages/database/tsup.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/index.ts', 'src/client.ts'],
+  entry: [
+    'src/index.ts',
+    'src/client.ts',
+    'src/repositories/analytics/index.ts',
+    'src/repositories/api/index.ts',
+    'src/repositories/auth/index.ts',
+    'src/repositories/base/index.ts',
+    'src/repositories/endpoint/index.ts',
+    'src/repositories/mock/index.ts',
+    'src/repositories/user/index.ts',
+    'src/repositories/workspace/index.ts',
+  ],
   format: ['cjs', 'esm'],
   dts: true,
   splitting: false,
